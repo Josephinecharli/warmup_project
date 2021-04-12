88987b84291743ab6d3d1607177cfc09c573b130
Josephine Charlie Passananti <josephinep@onix.cs.uchicago.edu>
2021-04-12 09:42:33 -0500

    patched :)

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -10,9 +10,12 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 
 # How close we will get to a wall with our front sensor.
-distance = 1
+distance = 1.3
 # How close we will stay to the walk while moving along it
 buffer = .4
+
+# keep track of whether a variable has been seen or not
+wall = False
 
 class FollowWall(object):
     """ This node walks the robot to a wall and turns """
@@ -20,6 +23,8 @@ class FollowWall(object):
     def __init__(self):
         # Start rospy node.
         rospy.init_node("follow_wall")
+
+        wall = False
 
         # Declare our node as a subscriber to the scan topic and
         #   set self.process_scan as the function to be used for callback.
@@ -33,6 +38,7 @@ class FollowWall(object):
         ang = Vector3()
         self.twist = Twist(linear=lin,angular=ang)
 
+
     def process_scan(self, data):
         # If we are far enough away from a wall, go straight, 
         # If we detect we are approaching a wall, start turning fast
@@ -44,20 +50,23 @@ class FollowWall(object):
         if data.ranges[0] < .3:
             #If robot is against a wall, backup so it can turn
             self.twist.linear.x = -0.3
+            print("1")
 
-        elif data.ranges[0] < .7 or data.ranges[320] < buffer:
+        elif (data.ranges[0] < .7 or data.ranges[320] < buffer):
             # If the front or side of the robot is very close to a wall, turn slightly
             self.twist.angular.z = .3
             self.twist.linear.x = .1
+            print("2")
 
         elif data.ranges[0] < distance:
             # If we are starting to get close to a wall, turn fast
             self.twist.angular.z = .8
             self.twist.linear.x = .3
+            print("3")
 
         elif data.ranges[0] >= distance:
             # Go forward if not close enough to a wall.
-            if data.ranges[320] > buffer:
+            if data.ranges[320] > buffer and data.ranges[320] < .8:
                 # If we are turning away from a wall, but not at a corner yet, 
                 # turn back slightly to maintain an even distance from wall
                 self.twist.angular.z = -.1
@@ -66,6 +75,7 @@ class FollowWall(object):
                 # If we are still close enough to wall, continue going straight
                 self.twist.linear.x = 0.2
                 self.twist.angular.z = 0
+            print("4")
 
         # Publish msg to cmd_vel.
         self.twist_pub.publish(self.twist)
